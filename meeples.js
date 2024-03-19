dba23758573e8d147a7278bf676bf6a2dac15176
Joy <32204909+andothergames@users.noreply.github.com>
2024-03-19 17:05:35 +0000

    meeple collision

--- meeples.js
+++ meeples.js
@@ -10,10 +10,14 @@ class Meeple {
 }
 
 let meepleGreen = new Meeple('forrestjump', 'aquamarine', 8, 8, false, false);
+//testing
+//let meepleGreen = new Meeple('forrestjump', 'aquamarine', 6, 9, false, false);
 let meepleGrey = new Meeple('ozzymosis', 'lightslategrey', 6, 6, false, false);
 let meepleBlue = new Meeple('bluebeamer', 'dodgerBlue', 9, 9, false, false);
 let meepleBrown = new Meeple('shortstop', 'tan', 11, 11, false, false);
 let meepleRed = new Meeple('sidestep', 'hotpink', 15, 15, false, false);
+//testing
+//let meepleRed = new Meeple('sidestep', 'hotpink', 2, 4, false, false);
 let meepleWhite = new Meeple('skewt', 'ghostwhite', 13, 13, false, false);
 let meepleYellow = new Meeple('mcedge', 'yellow', 4, 4, false, false);
 let meepleBlack = new Meeple('carbon', 'darkslategrey', 2, 2, false, false);
