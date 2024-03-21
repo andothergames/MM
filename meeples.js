4d199ef8fe067e6f116460433d4e8a8b43d1c500
Joy <32204909+andothergames@users.noreply.github.com>
2024-03-21 16:05:11 +0000

    abilityActive value

--- meeples.js
+++ meeples.js
@@ -1,24 +1,21 @@
 class Meeple {
-    constructor(name, color, xPos, yPos, isActive, abilityUsed, reachedTarget) {
+    constructor(name, color, xPos, yPos, isActive, abilityActive, abilityUsed, reachedTarget) {
         this.name = name
         this.color = color
         this.xPos = xPos
         this.yPos = yPos
         this.isActive = isActive
+        this.abilityActive = abilityActive
         this.abilityUsed = abilityUsed
         this.reachedTarget = reachedTarget
     }
 }
 
-let meepleGreen = new Meeple('forrestjump', 'aquamarine', 8, 8, false, false, false);
-//testing
-//let meepleGreen = new Meeple('forrestjump', 'aquamarine', 6, 9, false, false, false);
-let meepleGrey = new Meeple('ozzymosis', 'lightslategrey', 6, 6, false, false, false);
-let meepleBlue = new Meeple('bluebeamer', 'dodgerBlue', 9, 9, false, false, false);
-let meepleBrown = new Meeple('shortstop', 'tan', 11, 11, false, false, false);
-let meepleRed = new Meeple('sidestep', 'hotpink', 15, 15, false, false, false);
-//testing
-//let meepleRed = new Meeple('sidestep', 'hotpink', 2, 4, false, false, false);
-let meepleWhite = new Meeple('skewt', 'ghostwhite', 13, 13, false, false, false);
-let meepleYellow = new Meeple('mcedge', 'yellow', 4, 4, false, false, false);
-let meepleBlack = new Meeple('carbon', 'darkslategrey', 2, 2, false, false, false);
+let meepleGreen = new Meeple('forrestjump', 'aquamarine', 8, 8, false, false, false, false);
+let meepleGrey = new Meeple('ozzymosis', 'lightslategrey', 6, 6, false, false, false, false);
+let meepleBlue = new Meeple('bluebeamer', 'dodgerBlue', 9, 9, false, false, false, false);
+let meepleBrown = new Meeple('shortstop', 'tan', 11, 11, false, false, false, false);
+let meepleRed = new Meeple('sidestep', 'hotpink', 15, 15, false, false, false, false);
+let meepleWhite = new Meeple('skewt', 'ghostwhite', 13, 13, false, false, false, false);
+let meepleYellow = new Meeple('mcedge', 'yellow', 4, 4, false, false, false, false);
+let meepleBlack = new Meeple('carbon', 'darkslategrey', 2, 2, false, false, false, false);
